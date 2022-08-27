0003db5743326254882532424afcbf8d92358a8b
Alexander Akulich <akulichalexander@gmail.com>
2022-08-27 16:58:54 +0300

    render/matapply.cpp: Expand macros to actual methods

--- src/libdev/render/matapply.cpp
+++ src/libdev/render/matapply.cpp
@@ -183,87 +183,72 @@ void RenIUnlitMatApp::applyDirectToVertices
 
 // Defines a function which iterates over a vector of indices and applies the
 // lighting calculation to every vertex referred to by the indices.
-#define APPLICATOR_INDICES_FUNCTION(APP_TYPE)					\
-void APP_TYPE::applyViaIndices									\
-(																\
-	const ctl_min_memory_vector<Ren::VertexIdx>& indices,	    \
-	RenIFloatLightingBuffer*	liteBuf							\
-) const															\
-{															    \
-	RenIVertex*  vertices   = liteBuf->vertices();				\
-	const float* pR		    = liteBuf->rLamberts();				\
-	const float* pG		    = liteBuf->gLamberts();				\
-	const float* pB		    = liteBuf->bLamberts();				\
-	bool*        pApplied   = liteBuf->matAppliedFlags();		\
-	bool*        pUnderflow = liteBuf->underflowFlags();		\
-	bool*		 pPerVertex = liteBuf->perVertexMatFlags();		\
-																\
-	ctl_min_memory_vector<Ren::VertexIdx>::const_iterator it = indices.begin();		\
-	while (it != indices.end())														\
-	{																				\
-		if (!perVertexMats_ || !pPerVertex[*it])									\
-		{																			\
-			bool& applied = pApplied[*it];											\
-			if (!applied)															\
-			{																		\
-				applied = true;														\
-																					\
-				if (pUnderflow[*it])												\
-					vertices[*it].color = packedAmbient_;							\
-				else																\
-					applyToVertex(vertices[*it], pR[*it], pG[*it], pB[*it]);		\
-			}																		\
-		}																			\
-		++it;																		\
-	}																				\
+void RenILitMatApp::applyViaIndices( const ctl_min_memory_vector< Ren::VertexIdx >& indices,
+                                     RenIFloatLightingBuffer* liteBuf ) const
+{
+    RenIVertex* vertices = liteBuf->vertices();
+    const float* pR = liteBuf->rLamberts();
+    const float* pG = liteBuf->gLamberts();
+    const float* pB = liteBuf->bLamberts();
+    bool* pApplied = liteBuf->matAppliedFlags();
+    bool* pUnderflow = liteBuf->underflowFlags();
+    bool* pPerVertex = liteBuf->perVertexMatFlags();
+
+    ctl_min_memory_vector< Ren::VertexIdx >::const_iterator it = indices.begin();
+    while ( it != indices.end() )
+    {
+        if ( !perVertexMats_ || !pPerVertex[ *it ] )
+        {
+            bool& applied = pApplied[ *it ];
+            if ( !applied )
+            {
+                applied = true;
+
+                if ( pUnderflow[ *it ] )
+                    vertices[ *it ].color = packedAmbient_;
+                else
+                    applyToVertex( vertices[ *it ], pR[ *it ], pG[ *it ], pB[ *it ] );
+            }
+        }
+        ++it;
+    }
 }
 
 // Defines a function which iterates over a vector of vertices and applies the
 // lighting calculation to each vertex.
-#define APPLICATOR_DIRECT_VERTEX_FUNCTION(APP_TYPE)					\
-void APP_TYPE::applyDirectToVertices								\
-(																	\
-	RenIFloatLightingBuffer*	liteBuf,							\
-	size_t					nVertices								\
-) const																\
-{															    	\
-	RenIVertex*  vertices   = liteBuf->vertices();					\
-	const float* pR		    = liteBuf->rLamberts();					\
-	const float* pG		    = liteBuf->gLamberts();					\
-	const float* pB		    = liteBuf->bLamberts();					\
-	bool*        pApplied   = liteBuf->matAppliedFlags();			\
-	bool*        pUnderflow = liteBuf->underflowFlags();			\
-	bool*		 pPerVertex = liteBuf->perVertexMatFlags();			\
-																	\
-	for (size_t i=nVertices; i!=0; --i)								\
-	{																\
-		if (!perVertexMats_ || !(*pPerVertex))						\
-		{															\
-			bool& applied = *pApplied;								\
-			if (!applied)											\
-			{														\
-				applied = true;										\
-																	\
-				if (*pUnderflow)									\
-					vertices->color = packedAmbient_;				\
-				else												\
-					applyToVertex(*vertices, *pR, *pG, *pB);		\
-			}														\
-		}															\
-		++vertices;													\
-		++pR;														\
-		++pG;														\
-		++pB;														\
-		++pPerVertex;												\
-		++pApplied;													\
-		++pUnderflow;												\
-	}																\
+void RenILitMatApp::applyDirectToVertices( RenIFloatLightingBuffer* liteBuf, size_t nVertices ) const
+{
+    RenIVertex* vertices = liteBuf->vertices();
+    const float* pR = liteBuf->rLamberts();
+    const float* pG = liteBuf->gLamberts();
+    const float* pB = liteBuf->bLamberts();
+    bool* pApplied = liteBuf->matAppliedFlags();
+    bool* pUnderflow = liteBuf->underflowFlags();
+    bool* pPerVertex = liteBuf->perVertexMatFlags();
+
+    for ( size_t i = nVertices; i != 0; --i )
+    {
+        if ( !perVertexMats_ || !( *pPerVertex ) )
+        {
+            bool& applied = *pApplied;
+            if ( !applied )
+            {
+                applied = true;
+
+                if ( *pUnderflow )
+                    vertices->color = packedAmbient_;
+                else
+                    applyToVertex( *vertices, *pR, *pG, *pB );
+            }
+        }
+        ++vertices;
+        ++pR;
+        ++pG;
+        ++pB;
+        ++pPerVertex;
+        ++pApplied;
+        ++pUnderflow;
+    }
 }
-
-#define APPLICATOR_FUNCTIONS(T)				\
-	APPLICATOR_DIRECT_VERTEX_FUNCTION(T) 	\
-	APPLICATOR_INDICES_FUNCTION(T)
-
-APPLICATOR_FUNCTIONS(RenILitMatApp)
 
 /* End MATAPPLY.CPP *************************************************/
