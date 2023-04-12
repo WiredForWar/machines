WiredForWar/machines-archived

commit f5573167955714621dfd0ff230e9eb4379d2d6ab

Refactor startup to include the profiler init into the loading progress

src/projects/machines/sdlapp.cpp
@@ -221,8 +221,6 @@ bool SDLApp::clientStartup()
     SysRegistry::instance().currentStubKey("SOFTWARE\\Acclaim Entertainment\\Machines Demo");
 #endif
 
-    initSound();
-
     // Start to play a CD to give the poor user something to listen to whilst
     // the models load...
     initMusic();
@@ -233,14 +231,10 @@ bool SDLApp::clientStartup()
     SysMetaFile::encryptionType(SysMetaFile::ENCRYPTION_1);
 #endif
 
-    initProfiling();
-
     std::set_new_handler(newHandler);
 
     // Set the diretory to look for all files
     SysPathName::rootEnvironmentVariable("MACH_ROOT");
-
-    MachPhysData::instance();
 
     bool lobbyFlag = false;
 
@@ -463,13 +457,24 @@ bool SDLApp::clientStartup()
     initialiseGui(startedType, &progressIndicator);
 
     progressIndicator.report(100, 100);
-    progressIndicator.setLimits(0.18, 0.26);
+    progressIndicator.setLimits(0.18, 0.20);
+
+    initSound();
+
+    progressIndicator.setLimits(0.20, 0.35);
+    initProfiling(&progressIndicator);
+
+    progressIndicator.report(100, 100);
+    progressIndicator.setLimits(0.35, 0.37);
+    pStartupScreens_->initialiseVolumes();
+
+    MachPhysData::instance();
 
     MachPhysPreload::registerSounds();
     loadSounds();
 
     progressIndicator.report(100, 100);
-    progressIndicator.setLimits(0.26, 0.28);
+    progressIndicator.setLimits(0.37, 0.40);
 
     // Set up the texture search path.
     RenTexManager::PathNames searchList = RenTexManager::instance().searchList();
@@ -490,7 +495,7 @@ bool SDLApp::clientStartup()
     }
 
     progressIndicator.report(100, 100);
-    progressIndicator.setLimits(0.28, 0.71);
+    progressIndicator.setLimits(0.40, 0.71);
 
     if (noTexturePreload /*or lobbyFlag*/)
     {
@@ -672,12 +677,16 @@ void SDLApp::outputDebugInfo(const MexPoint2d& pos, const MexTransform3d& xform,
 #endif
 }
 
-void SDLApp::initProfiling()
+void SDLApp::initProfiling(IProgressReporter* pReporter)
 {
+    spdlog::info("Initializing profiler...");
+
     double profileInterval = 50.0;
     char* pMs = getenv("PROFILE_RATE");
     if (pMs)
         profileInterval = atof(pMs);
+
+    ProProfiler::instance(pReporter);
     ProProfiler::instance().traceInterval(profileInterval / 1000.0);
 
     HAL_STREAM("SDLApp::clientStartup\n");

src/libdev/machgui/startup.cpp
@@ -273,8 +273,6 @@ MachGuiStartupScreens::MachGuiStartupScreens(
     MachGuiInGameChatMessages::instance().initialise(pMessageBroker_, this);
     MachLogRecentEventsManager::instance().setCameras(pInGameScreen_->cameras());
 
-    initialiseVolumes();
-
     pReporter->report(100, 100); // 100% of gui stuff done
     if (getenv("CB_DISABLE_NETWORK_TO"))
     {

src/projects/machines/sdlapp.hpp
@@ -54,7 +54,7 @@ private:
 
     void outputDebugInfo(const MexPoint2d& pos, const MexTransform3d& xform, MachActor* pActor);
 
-    void initProfiling();
+    void initProfiling(IProgressReporter* pReporter);
 
     // Sim
     void checkSuspendSimManager();

WiredForWar/machines

commit 30c126984c504e93d9e939f6d94b8667a6730fd8

Fix music initialization
Regression after f5573167955714621dfd0ff230e9eb4379d2d6ab.

src/projects/machines/sdlapp.cpp
@@ -222,10 +222,6 @@ bool SDLApp::clientStartup()
     SysRegistry::instance().currentStubKey("SOFTWARE\\Acclaim Entertainment\\Machines Demo");
 #endif
 
-    // Start to play a CD to give the poor user something to listen to whilst
-    // the models load...
-    initMusic();
-
 #ifdef DEMO
     SysMetaFile::encryptionType(SysMetaFile::ENCRYPTION_2);
 #else
@@ -484,6 +480,10 @@ bool SDLApp::clientStartup()
     progressIndicator.setLimits(0.18, 0.20);
 
     initSound();
+
+    // Start to play a CD to give the poor user something to listen to whilst
+    // the models load...
+    initMusic();
 
     progressIndicator.setLimits(0.20, 0.35);
     initProfiling(&progressIndicator);