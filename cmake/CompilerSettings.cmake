57486ef788dec4209cb4e770eaf3ec25cc36132e
Alexander Akulich <akulichalexander@gmail.com>
2023-11-25 10:19:32 +0300

    CompilerSettings: Disable 'warnings as errors' for clang

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -55,8 +55,8 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         message(FATAL_ERROR "${PROJECT_NAME} requires Clang 3.1 or greater.")
     endif()
 
-    set(NORMAL_CXX_FLAGS "${MACHINES_COMPILER_BIT_MODE} -Wall -Werror -Wold-style-cast -pedantic-errors -Wmissing-prototypes")
-    set(NORMAL_CXX_FLAGS "${NORMAL_CXX_FLAGS} -Wno-error=deprecated-declarations") # updated version of physfs is not available on some platforms so we keep using deprecated functions, see #958
+    #set(NORMAL_CXX_FLAGS "-Wall -Werror -Wold-style-cast -pedantic-errors -Wmissing-prototypes")
+    set(NORMAL_CXX_FLAGS "${MACHINES_COMPILER_BIT_MODE} ${NORMAL_CXX_FLAGS} -Wno-error=deprecated-declarations") # updated version of physfs is not available on some platforms so we keep using deprecated functions, see #958
     set(RELEASE_CXX_FLAGS "-O2")
     set(DEBUG_CXX_FLAGS "-g -O0")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
