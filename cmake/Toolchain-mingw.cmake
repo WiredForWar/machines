0757629defbcaab14b3caa2a8580f3459b215ef3
Alexander Akulich <akulichalexander@gmail.com>
2024-03-23 13:12:54 +0300

    CMake/Toolchain-mingw: Rely on externally defined dependencies dir

--- cmake/Toolchain-mingw.cmake
+++ cmake/Toolchain-mingw.cmake
@@ -6,9 +6,9 @@ set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
-set(WINDOWS_DEPENDENCIES_DIR ${PROJECT_SOURCE_DIR}/windows)
-set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} /usr/lib/gcc/${TOOLCHAIN_PREFIX}/9.3-posix ${WINDOWS_DEPENDENCIES_DIR})
-set(CMAKE_PREFIX_PATH ${WINDOWS_DEPENDENCIES_DIR})
+message(STATUS "WINDOWS_DEPENDENCIES_DIR: ${WINDOWS_DEPENDENCIES_DIR}")
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} ${WINDOWS_DEPENDENCIES_DIR})
+set(CMAKE_PREFIX_PATH "${WINDOWS_DEPENDENCIES_DIR}")
 
 # modify default behavior of FIND_XXX() commands to
 # search for headers/libs in the target environment and
