4e1fb035958a7f4447228104b63916d45598fb74
Alexander Akulich <akulichalexander@gmail.com>
2024-04-06 03:08:19 +0300

    base/pertypes: Add missing include

--- src/libdev/base/private/pertypes.hpp
+++ src/libdev/base/private/pertypes.hpp
@@ -10,12 +10,14 @@
 #ifndef _PER_INTERNAL_PERTYPES_HPP
 #define _PER_INTERNAL_PERTYPES_HPP
 
-using PerIdentifier = uint32;
+#include <cstdint>
+
+using PerIdentifier = uint32_t;
 
 //  There seems to be some problem with storing pointers in a map so we use a
 //  type that should allow error free casting in both directions.
 
-using PerMapPtrType = size_t;
+using PerMapPtrType = std::size_t;
 
 class PerIstream;
 class PerOstream;
