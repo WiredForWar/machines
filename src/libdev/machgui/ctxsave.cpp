452c6cf245ca1e61900c9ecbfc387f18f6762d6d
Alexander Akulich <akulichalexander@gmail.com>
2025-06-22 00:58:18 +0300

    MachGuiCtxSave::saveGame: Fix printf format

--- src/libdev/machgui/ctxsave.cpp
+++ src/libdev/machgui/ctxsave.cpp
@@ -431,7 +431,7 @@ bool MachGuiCtxSave::saveGame(const std::string& saveDisplayName)
     {
         char buffer[20];
 
-        sprintf(buffer, "%04ld", count);
+        sprintf(buffer, "%04zu", count);
 
         // savePathName = string( "savegame/save" ) + buffer + ".sav";
         savePathName = SysPathName(std::string("savegame/save") + buffer + ".sav");
