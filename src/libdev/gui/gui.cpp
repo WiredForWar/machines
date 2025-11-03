c9b6d9905e33af4d0c3ba145efa69b8b13d84f30
Alexander Akulich <akulichalexander@gmail.com>
2025-11-04 01:01:24 +0300

    gui: Add string join() helper template

--- src/libdev/gui/gui.cpp
+++ src/libdev/gui/gui.cpp
@@ -272,3 +272,42 @@ void Gui::setUiScaleFactor(MATHEX_SCALAR scale)
 }
 
 /* End **************************************************************/
+
+template<std::forward_iterator Iterator>
+std::string Gui::join(Iterator beginIt, Iterator endIt, std::string_view sep)
+{
+    std::string result;
+    if (beginIt != endIt)
+    {
+        std::size_t finalSize{};
+        for (auto it = beginIt; it != endIt; ++it)
+        {
+            const std::string_view v = *it;
+            finalSize += v.size();
+        }
+        finalSize += std::distance(beginIt, endIt) * sep.size();
+
+        result.reserve(finalSize);
+        result += *beginIt;
+
+        for (auto it = std::next(beginIt); it != endIt; ++it)
+        {
+            result += sep;
+            const std::string_view v = *it;
+            result += v;
+        }
+    }
+
+    return result;
+}
+
+namespace
+{
+
+using StrIt = std::vector<std::string>::iterator;
+using StrConstIt = std::vector<std::string>::const_iterator;
+
+} // namespace
+
+template std::string Gui::join<>(StrIt, StrIt, std::string_view);
+template std::string Gui::join<>(StrConstIt, StrConstIt, std::string_view);
