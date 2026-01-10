9fc423b819508c0af10d9bf3d9f839d9bd3e070d
Alexander Akulich <akulichalexander@gmail.com>
2026-01-11 00:18:04 +0300

    Implement rudimentary Console class API

--- src/libdev/system/IConsole.hpp
+++ src/libdev/system/IConsole.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
+#include <functional>
+#include <string>
+#include <string_view>
+
 namespace System
 {
 
 class IConsole
 {
+public:
+    struct CommandMetadata
+    {
+        std::string name{};
+        std::string description{};
+    };
+
+    struct CommandRequest
+    {
+        std::string name{};
+        std::string rawLine{};
+    };
+
+    using CommandHandler = std::function<void(const CommandRequest&)>;
+
+    virtual ~IConsole() = default;
+
+    virtual bool registerCommand(const CommandMetadata& metadata, CommandHandler handler) = 0;
+
+    virtual bool submit(std::string_view line) = 0;
 };
 
 } // namespace System
