971f694c2325b6aaf03ece7504b4fb8139167728
Alexander Akulich <akulichalexander@gmail.com>
2026-02-08 14:01:25 +0300

    render: Add cull-face backend command

--- src/libdev/render/internal/BackendCommands.hpp
+++ src/libdev/render/internal/BackendCommands.hpp
@@ -53,6 +53,11 @@ struct BackendCommandSetBlendState
     BackendBlendFactor dstFactor{BackendBlendFactor::Zero};
 };
 
+struct BackendCommandSetCullFace
+{
+    bool enabled{};
+};
+
 struct BackendCommandSetAlphaTest
 {
     bool enabled{};
@@ -75,6 +80,7 @@ using BackendCommand = std::variant<
     BackendCommandDraw,
     BackendCommandDrawIndexed,
     BackendCommandSetBlendState,
+    BackendCommandSetCullFace,
     BackendCommandSetAlphaTest,
     BackendCommandSetDepthMask,
     BackendCommandSetDepthTest>;
@@ -106,6 +112,11 @@ inline BackendCommand drawIndexed(
 inline BackendCommand setBlendStateEnabled(BackendBlendFactor srcFactor, BackendBlendFactor dstFactor)
 {
     return BackendCommandSetBlendState{ .enabled = true, .srcFactor = srcFactor, .dstFactor = dstFactor };
+}
+
+inline BackendCommand setCullFace(bool enabled)
+{
+    return BackendCommandSetCullFace{enabled};
 }
 
 inline BackendCommand setAlphaTestEnabled(float reference)
