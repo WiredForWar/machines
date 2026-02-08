0d1882cd2882a1974070439d3b32f620d9192815
Alexander Akulich <akulichalexander@gmail.com>
2026-02-08 11:22:33 +0300

    render: Add draw backend command

--- src/libdev/render/Device.cpp
+++ src/libdev/render/Device.cpp
@@ -2047,9 +2047,7 @@ void RenDevice::renderScreenspace(
     glDisable(GL_DEPTH_TEST);
     //    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    GLenum mode = Ren::OpenGL::getDrawMode(topology);
-    // Draw call
-    glDrawArrays(mode, 0, nVertices);
+    recordCommand(Ren::Command::draw(topology, 0, nVertices));
 
     glDisable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
@@ -2324,9 +2322,7 @@ void RenDevice::renderPrimitive(
         (void*)0                      // array buffer offset
     );
      */
-    // Draw the triangles !
-    GLenum mode = Ren::OpenGL::getDrawMode(topology);
-    glDrawArrays(mode, 0, nVertices);
+    recordCommand(Ren::Command::draw(topology, 0, nVertices));
 
     glDisableVertexAttribArray(glVertexPosition_modelspaceID_);
     glDisableVertexAttribArray(glVertex_modelspaceUVID_);
