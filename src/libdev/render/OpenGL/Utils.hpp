74ab8c296a39bda5f39f402467a61c0a750b84dd
Alexander Akulich <akulichalexander@gmail.com>
2026-02-08 12:04:29 +0300

    render: Format OpenGL/Utils code

--- src/libdev/render/OpenGL/Utils.hpp
+++ src/libdev/render/OpenGL/Utils.hpp
@@ -9,63 +9,58 @@ namespace Ren
 
 namespace OpenGL
 {
-    class ScopedEnable
+class ScopedEnable
+{
+public:
+    explicit ScopedEnable(GLenum capability)
+        : capability_(capability)
     {
-    public:
-        explicit ScopedEnable(GLenum capability)
-            : capability_(capability)
-        {
-            glEnable(capability_);
-        }
-
-        ~ScopedEnable()
-        {
-            glDisable(capability_);
-        }
-
-    private:
-        GLenum capability_{};
-    };
-
-    class ScopedDisable
-    {
-    public:
-        explicit ScopedDisable(GLenum capability)
-            : capability_(capability)
-        {
-            glDisable(capability_);
-        }
-
-        ~ScopedDisable()
-        {
-            glEnable(capability_);
-        }
-
-    private:
-        GLenum capability_{};
-    };
-
-    constexpr GLenum getDrawMode(Ren::PrimitiveTopology topology)
-    {
-        switch (topology)
-        {
-            case PrimitiveTopology::Points:
-                return GL_POINTS;
-            case PrimitiveTopology::Lines:
-                return GL_LINES;
-            case PrimitiveTopology::LineStrip:
-                return GL_LINE_STRIP;
-            case PrimitiveTopology::Triangles:
-                return GL_TRIANGLES;
-            case PrimitiveTopology::TriangleStrip:
-                return GL_TRIANGLE_STRIP;
-            case PrimitiveTopology::TriangleFan:
-                return GL_TRIANGLE_FAN;
-        }
-
-        // UNREACHABLE
-        return GL_NONE;
+        glEnable(capability_);
     }
+
+    ~ScopedEnable() { glDisable(capability_); }
+
+private:
+    GLenum capability_{};
+};
+
+class ScopedDisable
+{
+public:
+    explicit ScopedDisable(GLenum capability)
+        : capability_(capability)
+    {
+        glDisable(capability_);
+    }
+
+    ~ScopedDisable() { glEnable(capability_); }
+
+private:
+    GLenum capability_{};
+};
+
+constexpr GLenum getDrawMode(Ren::PrimitiveTopology topology)
+{
+    switch (topology)
+    {
+    case PrimitiveTopology::Points:
+        return GL_POINTS;
+    case PrimitiveTopology::Lines:
+        return GL_LINES;
+    case PrimitiveTopology::LineStrip:
+        return GL_LINE_STRIP;
+    case PrimitiveTopology::Triangles:
+        return GL_TRIANGLES;
+    case PrimitiveTopology::TriangleStrip:
+        return GL_TRIANGLE_STRIP;
+    case PrimitiveTopology::TriangleFan:
+        return GL_TRIANGLE_FAN;
+    }
+
+    // UNREACHABLE
+    return GL_NONE;
+}
+
 } // namespace OpenGL
 
 } // namespace Ren
