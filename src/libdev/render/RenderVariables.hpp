5d0f1f74f729bcd7a1b58bd290ae3a304d7d2a2d
Alexander Akulich <akulichalexander@gmail.com>
2026-02-15 16:11:24 +0300

    render: Add ShadowQuality config var

--- src/libdev/render/RenderVariables.hpp
+++ src/libdev/render/RenderVariables.hpp
@@ -3,10 +3,12 @@
 #include "system/Variable.hpp"
 
 enum class LightingMode;
+enum class ShadowQuality;
 
 namespace Config
 {
 
 extern Variable<LightingMode> gfxLightingMode;
+extern Variable<ShadowQuality> gfxShadowQuality;
 
 } // namespace Config
