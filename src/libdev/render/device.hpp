a52c55810c3189f6a376bb54fc062d73b170c338
Alexander Akulich <akulichalexander@gmail.com>
2026-02-17 03:21:16 +0300

    render: Integrate post-process tone mapping

--- src/libdev/render/device.hpp
+++ src/libdev/render/device.hpp
@@ -416,6 +416,7 @@ private:
     void addInterference();
     void graduatedNoisePolygon(const Ren::Rect& area, double minAlpha, double maxAlpha);
     void uniformNoisePolygon(const Ren::Rect& area, double maxAlpha);
+    void blitPostProcess();
     void commonEndFrame();
 
     bool setVSync(bool enabled);
