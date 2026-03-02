4ce287f9413a55b861e3033dbb57d4cfd4bf15f7
Alexander Akulich <akulichalexander@gmail.com>
2026-03-02 03:54:29 +0300

    GuiSingleLineEditBox: Add clear()

--- src/libdev/gui/editbox.cpp
+++ src/libdev/gui/editbox.cpp
@@ -307,6 +307,11 @@ void GuiSingleLineEditBox::caretColour(const GuiColour& colour)
     caretColour_ = colour;
 }
 
+void GuiSingleLineEditBox::clear()
+{
+    setText({});
+}
+
 void GuiSingleLineEditBox::setTextChangedCallback(Callback callback)
 {
     textChangedCallback_ = callback;
