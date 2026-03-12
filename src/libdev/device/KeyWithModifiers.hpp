293765286b342e5c12dc8d24cdfb4be52fab379f
Alexander Akulich <akulichalexander@gmail.com>
2026-03-12 23:38:03 +0300

    KeyWithModifiers: Add some constexpr keywords

--- src/libdev/device/KeyWithModifiers.hpp
+++ src/libdev/device/KeyWithModifiers.hpp
@@ -9,7 +9,7 @@ public:
     static constexpr Underlying Alt = static_cast<Underlying>(Device::KeyModifier::Alt);
     static constexpr Underlying Ctrl = static_cast<Underlying>(Device::KeyModifier::Ctrl);
     static constexpr Underlying Shift = static_cast<Underlying>(Device::KeyModifier::Shift);
-    static constexpr Underlying All = Alt|Ctrl|Shift;
+    static constexpr Underlying AllMask = static_cast<Underlying>(Alt | Ctrl | Shift);
 
     static constexpr Device::KeyModifier Order[] = {
         Device::KeyModifier::Ctrl,
@@ -18,52 +18,55 @@ public:
     };
 
     KeyModifierFlags() = default;
-    KeyModifierFlags(Device::KeyModifier modifier);
-    explicit KeyModifierFlags(Underlying v);
+    constexpr KeyModifierFlags(Device::KeyModifier modifier);
+    constexpr explicit KeyModifierFlags(Underlying v);
 
-    static KeyModifierFlags fromCtrlAltShiftState(bool ctrl, bool alt, bool shift);
+    static constexpr KeyModifierFlags fromCtrlAltShiftState(bool ctrl, bool alt, bool shift);
+    static const KeyModifierFlags All;
 
     bool operator==(const KeyModifierFlags&) const = default;
 
-    KeyModifierFlags operator|(Device::KeyModifier modifier) const noexcept;
-    KeyModifierFlags operator&(const KeyModifierFlags flags) const noexcept;
+    constexpr KeyModifierFlags operator|(Device::KeyModifier modifier) const noexcept;
+    constexpr KeyModifierFlags operator&(const KeyModifierFlags flags) const noexcept;
 
-    operator bool() const;
+    constexpr operator bool() const noexcept;
 
     Underlying value {};
 };
 
-inline KeyModifierFlags::KeyModifierFlags(Device::KeyModifier modifier)
+inline constexpr KeyModifierFlags::KeyModifierFlags(Device::KeyModifier modifier)
     : value(static_cast<Underlying>(modifier))
 {
 }
 
-inline KeyModifierFlags::KeyModifierFlags(Underlying v)
+inline constexpr KeyModifierFlags::KeyModifierFlags(Underlying v)
     : value(v)
 {
 }
 
-inline KeyModifierFlags KeyModifierFlags::fromCtrlAltShiftState(bool ctrl, bool alt, bool shift)
+inline constexpr KeyModifierFlags KeyModifierFlags::fromCtrlAltShiftState(bool ctrl, bool alt, bool shift)
 {
-    return KeyModifierFlags(Ctrl * ctrl | Alt * alt | Shift * shift);
+    return KeyModifierFlags(Underlying(Ctrl * ctrl | Alt * alt | Shift * shift));
 }
 
-inline KeyModifierFlags KeyModifierFlags::operator|(Device::KeyModifier modifier) const noexcept
+inline const KeyModifierFlags KeyModifierFlags::All = KeyModifierFlags(AllMask);
+
+inline constexpr KeyModifierFlags KeyModifierFlags::operator|(Device::KeyModifier modifier) const noexcept
 {
-    return Device::KeyModifier(value | static_cast<Underlying>(modifier));
+    return KeyModifierFlags(Underlying(value | static_cast<Underlying>(modifier)));
 }
 
-inline KeyModifierFlags KeyModifierFlags::operator&(const KeyModifierFlags flags) const noexcept
+inline constexpr KeyModifierFlags KeyModifierFlags::operator&(const KeyModifierFlags flags) const noexcept
 {
     return KeyModifierFlags(value & flags.value);
 }
 
-inline KeyModifierFlags::operator bool() const
+inline constexpr KeyModifierFlags::operator bool() const noexcept
 {
     return value != 0;
 }
 
-inline KeyModifierFlags operator|(Device::KeyModifier mod1, Device::KeyModifier mod2)
+inline constexpr KeyModifierFlags operator|(Device::KeyModifier mod1, Device::KeyModifier mod2)
 {
     return KeyModifierFlags(mod1) | mod2;
 }
@@ -74,64 +77,63 @@ public:
     using Underlying = uint16_t;
 
     KeyWithModifiers() = default;
-    KeyWithModifiers(const KeyWithModifiers&) = default;
 
     auto operator<=>(const KeyWithModifiers&) const = default;
     bool operator==(const KeyWithModifiers&) const = default;
-    bool operator==(Underlying v) const
+    constexpr bool operator==(Underlying v) const noexcept
     {
         return value == v;
     }
 
-    KeyWithModifiers(Device::KeyCode code, KeyModifierFlags modFlags = {})
+    constexpr KeyWithModifiers(Device::KeyCode code, KeyModifierFlags modFlags = {})
         : value(static_cast<Underlying>(code) | modFlags.value)
     {
     }
 
-    Device::KeyCode keyCode() const noexcept
+    constexpr Device::KeyCode keyCode() const noexcept
     {
         return Device::getKeyWithoutModifiers(value);
     }
 
-    KeyModifierFlags modifiers() const noexcept
+    constexpr KeyModifierFlags modifiers() const noexcept
     {
-        return static_cast<Device::KeyModifier>(value & KeyModifierFlags::All);
+        return KeyModifierFlags(KeyModifierFlags::Underlying(value & KeyModifierFlags::AllMask));
     }
 
-    KeyWithModifiers operator|(KeyModifierFlags flags) const noexcept
+    constexpr KeyWithModifiers operator|(KeyModifierFlags flags) const noexcept
     {
-        return KeyWithModifiers(value | flags.value);
+        return KeyWithModifiers(Underlying(value | flags.value));
     }
 
-    Underlying operator&(const KeyModifierFlags flags) const noexcept
+    constexpr Underlying operator&(const KeyModifierFlags flags) const noexcept
     {
         return value & flags.value;
     }
 
-    Underlying operator&(const KeyWithModifiers keyWithMods) const noexcept
+    constexpr Underlying operator&(const KeyWithModifiers keyWithMods) const noexcept
     {
         return value & keyWithMods.value;
     }
 
 private:
-    explicit KeyWithModifiers(Underlying v)
+    constexpr explicit KeyWithModifiers(Underlying v)
         : value(v)
     {
     }
     Underlying value {};
 };
 
-inline KeyWithModifiers operator|(Device::KeyCode code, Device::KeyModifier mod)
+inline constexpr KeyWithModifiers operator|(Device::KeyCode code, Device::KeyModifier mod)
 {
     return KeyWithModifiers(code, mod);
 }
 
-inline KeyWithModifiers operator|(Device::KeyCode code, KeyModifierFlags mods)
+inline constexpr KeyWithModifiers operator|(Device::KeyCode code, KeyModifierFlags mods)
 {
     return KeyWithModifiers(code, mods);
 }
 
-inline bool operator==(KeyWithModifiers::Underlying u, const KeyWithModifiers keyWithMods)
+inline constexpr bool operator==(KeyWithModifiers::Underlying u, const KeyWithModifiers keyWithMods)
 {
     return keyWithMods == u;
 }
